37a0c732b427953f3e077db6d739e213d93ba988
VSadeev <metel20@gmail.com>
2024-01-25 18:50:47 +0300

    MainViewModel

--- app/src/main/java/com/kliachenko/unscramblegame/GameApp.kt
+++ app/src/main/java/com/kliachenko/unscramblegame/GameApp.kt
@@ -1,30 +1,36 @@
 package com.kliachenko.unscramblegame
 
 import android.app.Application
+import androidx.lifecycle.ViewModel
 import com.kliachenko.unscramblegame.game.GameRepository
 import com.kliachenko.unscramblegame.game.GameViewModel
 import com.kliachenko.unscramblegame.game.PermanentStorage
 import com.kliachenko.unscramblegame.game.Shuffle
+import com.kliachenko.unscramblegame.main.MainViewModel
+import java.lang.IllegalStateException
 
 class GameApp : Application() {
 
-    private lateinit var viewModel: GameViewModel
 
-    fun viewModel() = viewModel
-
-    override fun onCreate() {
-        super.onCreate()
-
+    fun <T : ViewModel> viewModel(clasz: Class<out T>): T {
         val isRelease = !BuildConfig.DEBUG
         val shuffle = if (isRelease) Shuffle.Base() else Shuffle.Reversed()
         val wordsCount = if (isRelease) 10 else 2
 
-        viewModel = GameViewModel(
-            GameRepository.Base(
-                shuffle = shuffle,
-                wordsCount = wordsCount,
-                permanentStorage = PermanentStorage.Base(this)
+        return when (clasz) {
+            MainViewModel::class.java -> MainViewModel()
+            GameViewModel::class.java -> GameViewModel(
+                GameRepository.Base(
+                    shuffle = shuffle,
+                    wordsCount = wordsCount,
+                    permanentStorage = PermanentStorage.Base(this)
+                )
             )
-        )
+
+            else -> {throw IllegalStateException()}
+        } as T
+
     }
+
 }
+
--- app/src/main/java/com/kliachenko/unscramblegame/main/MainActivity.kt
+++ app/src/main/java/com/kliachenko/unscramblegame/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kliachenko.unscramblegame.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kliachenko.unscramblegame.GameApp
 import com.kliachenko.unscramblegame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val viewModel:MainViewModel = (application as GameApp).viewModel(MainViewModel::class.java)
 
 
 
